0b7f84fcf57ff473d5d716fe56b03022e91934ad
Daniel Pan <danielcpan@gmail.com>
2019-08-10 22:32:43 -0700

    Build

--- server/app.js
+++ server/app.js
@@ -30,6 +30,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // const { createTestData } = require('./seeders/testData');
 // createTestData()
 
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
 // Mount all routes on /api path
 app.use('/api', routes);
 
@@ -46,15 +48,9 @@ app.use((err, req, res, next) => {
   return next(err);
 });
 
-console.log(path.resolve(__dirname, "../", "client", "build", "index.html"))
-if (process.env.NODE_ENV === "production") {
-  console.log("we got inside here!")
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../", "client", "build", "index.html"));
-  });
-}
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
 
 // Catch 404 and forward to Error Handler
 app.use((req, res, next) => {
